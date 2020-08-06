cdbe9206dfbff2f38b216124d407d32369592208
gns088 <ganeshram@hivepro.cloud>
2020-08-06 17:11:50 +0530

    @Kimaya added

--- projects/ngx-paypal-lib/src/lib/services/paypal-script.service.ts
+++ projects/ngx-paypal-lib/src/lib/services/paypal-script.service.ts
@@ -3,75 +3,77 @@ import { Injectable } from '@angular/core';
 import { IPayPalUrlConfig, IQueryParam } from '../models/paypal-models';
 import { ScriptService } from './script.service';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class PayPalScriptService {
 
-    private readonly paypalWindowName = 'paypal';
+  private readonly paypalWindowName = 'paypal';
 
 
-    constructor(
-        protected scriptService: ScriptService,
-    ) {
+  constructor(
+    protected scriptService: ScriptService,
+  ) {
+  }
+
+  registerPayPalScript(config: IPayPalUrlConfig, onReady: (payPalApi: any) => void): void {
+    this.scriptService.registerScript(this.getUrlForConfig(config), this.paypalWindowName, onReady);
+  }
+
+  destroyPayPalScript(): void {
+    this.scriptService.cleanup(this.paypalWindowName);
+  }
+
+  private getUrlForConfig(config: IPayPalUrlConfig): string {
+    const params: IQueryParam[] = [
+      {
+        name: 'client-id',
+        value: config.clientId
+      }
+    ];
+
+    if (config.currency) {
+      params.push({
+        name: 'currency',
+        value: config.currency
+      });
     }
 
-    registerPayPalScript(config: IPayPalUrlConfig, onReady: (payPalApi: any) => void): void {
-        this.scriptService.registerScript(this.getUrlForConfig(config), this.paypalWindowName, onReady);
+    if (config.commit) {
+      params.push({
+        name: 'commit',
+        value: config.commit
+      });
     }
 
-    destroyPayPalScript(): void {
-        this.scriptService.cleanup(this.paypalWindowName);
+    if (config.vault) {
+      params.push({
+        name: 'vault',
+        value: config.vault
+      });
     }
 
-    private getUrlForConfig(config: IPayPalUrlConfig): string {
-        const params: IQueryParam[] = [
-            {
-                name: 'client-id',
-                value: config.clientId
-            }
-        ];
-
-        if (config.currency) {
-            params.push({
-                name: 'currency',
-                value: config.currency
-            });
-        }
-
-        if (config.commit) {
-            params.push({
-                name: 'commit',
-                value: config.commit
-            });
-        }
-
-        if (config.vault) {
-            params.push({
-                name: 'vault',
-                value: config.vault
-            });
-        }
-
-        if (config.extraParams) {
-            params.push(...config.extraParams);
-        }
-
-        return `https://www.paypal.com/sdk/js${this.getQueryString(params)}`;
+    if (config.extraParams) {
+      params.push(...config.extraParams);
     }
 
-    private getQueryString(queryParams: IQueryParam[]): string {
-        let queryString = '';
+    return `https://www.paypal.com/sdk/js${this.getQueryString(params)}`;
+  }
 
-        for (let i = 0; i < queryParams.length; i++) {
-            const queryParam = queryParams[i];
-            if (i === 0) {
-                queryString += '?';
-            } else {
-                queryString += '&';
-            }
+  private getQueryString(queryParams: IQueryParam[]): string {
+    let queryString = '';
 
-            queryString += `${queryParam.name}=${queryParam.value}`;
-        }
+    for (let i = 0; i < queryParams.length; i++) {
+      const queryParam = queryParams[i];
+      if (i === 0) {
+        queryString += '?';
+      } else {
+        queryString += '&';
+      }
 
-        return queryString;
+      queryString += `${queryParam.name}=${queryParam.value}`;
     }
+
+    return queryString;
+  }
 }
